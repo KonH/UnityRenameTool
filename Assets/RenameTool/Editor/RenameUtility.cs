d2d9b2504ba750c23536df0953a558831e720016
KonH <konh@yandex.ru>
2017-02-27 21:35:48 +0700

    Utility classes: - Disposable layout wrappers for experimental purposes; - ObservedTextField with onChanged callback;

---
+++ Assets/RenameTool/Editor/RenameUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityRenameTool {
+	class VerticalLayout : IDisposable {
+		public VerticalLayout(params GUILayoutOption[] options) {
+			GUILayout.BeginVertical(options);
+		}
+
+		public void Dispose() {
+			GUILayout.EndVertical();
+		}
+	}
+
+	class HorizontalLayout : IDisposable {
+		public HorizontalLayout(params GUILayoutOption[] options) {
+			GUILayout.BeginHorizontal(options);
+		}
+
+		public void Dispose() {
+			GUILayout.EndHorizontal();
+		}
+	}
+
+	class ObservedTextField {
+		
+		string _text = "";
+		Action<string> _onChanged = null;
+
+		public ObservedTextField(Action<string> onChanged) {
+			_onChanged = onChanged;
+		}
+
+		public void Read() {
+			var tempText = GUILayout.TextField(_text);
+			var changed = (tempText != _text);
+			_text = tempText;
+			if( changed ) {
+				_onChanged(_text);
+			}
+		}
+	}
+}
