481748432c8757270126ddf0a5ee78a043404394
KonH <konh@yandex.ru>
2017-02-28 21:08:17 +0700

    RenameTool to rename gameObjects based on some function;

---
+++ Assets/RenameTool/Editor/RenameTool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityRenameTool.Editor {
+	public static class RenameTool {
+		
+		public static void Rename(GameObject go, Func<string, string> converter) {
+			if( go ) {
+				var so = new SerializedObject(go);
+				var nameProp = so.FindProperty("m_Name");
+				nameProp.stringValue = converter(nameProp.stringValue);
+				so.ApplyModifiedProperties();
+			}
+		}
+
+		public static void Rename(GameObject[] gos, Func<string, string> converter) {
+			for( int i = 0; i < gos.Length; i++ ) {
+				Rename(gos[i], converter);
+			}
+		}
+	}
+}
